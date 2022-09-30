c5e7601948289205e80dba2f0c9af9349c5acc9b
Nazira-Jesmin-Lina <83658996+Nazira-Jesmin-Lina@users.noreply.github.com>
2022-09-30 22:52:55 +0600

    sign up

--- app/src/main/java/com/example/firebaseauth/Signup.java
+++ app/src/main/java/com/example/firebaseauth/Signup.java
@@ -1,14 +1,41 @@
 package com.example.firebaseauth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class Signup extends AppCompatActivity {
+    private boolean pass_show1 = false;
+    private boolean pass_show2 = false;
+
+    private String emailpattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    private EditText name, email, mobile, pass1, pass2;
+
+    private AppCompatButton signup_button;
+    private TextView login;
+    ProgressDialog progressDialog;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        name = findViewById(R.id.sup_name);
+        email = findViewById(R.id.sup_email);
+        mobile = findViewById(R.id.sup_mobile);
+        pass1 = findViewById(R.id.sup_pass);
+        pass2 = findViewById(R.id.sup_pass_cnfrm);
+        progressBar = findViewById(R.id.progressBar);
+        signup_button = findViewById(R.id.sup_button);
+        login = findViewById(R.id.login_page_back);
+        progressDialog = new ProgressDialog(this);
+
     }
 }
