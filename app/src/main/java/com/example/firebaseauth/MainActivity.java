1d71af64cdaf66fec79c0b74460d64700f024aaf
Nazira-Jesmin-Lina <83658996+Nazira-Jesmin-Lina@users.noreply.github.com>
2022-09-30 23:15:45 +0600

    sign up

--- app/src/main/java/com/example/firebaseauth/MainActivity.java
+++ app/src/main/java/com/example/firebaseauth/MainActivity.java
@@ -14,12 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         next=findViewById(R.id.button);
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getSupportActionBar().hide();
-                startActivity(new Intent(MainActivity.this, Login.class));
-            }
-        });
+
     }
 }
